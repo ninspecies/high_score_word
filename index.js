2c9e6e11a10230afac97eefa7f92f57a33d15bcd
Ninian Bond <ninrhythm@gmail.com>
2025-12-27 18:54:11 +0000

    solved high_score_word and added instructions

---
+++ index.js
@@ -0,0 +1,20 @@
+function high(x){
+  let words = x.split(" ")
+  let alpha = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13,
+              "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25,
+              "z": 26}
+  let total = 0
+  let index = ""
+  words.forEach( word => {
+    let temp = 0
+    for (let i = 0; i < word.length; i++) {
+      temp += alpha[word[i]]
+    }
+    if (temp > total) {
+      total = temp
+      index = word
+    }
+    
+  })
+  return index
+}
